13f8bf4c8631930b8d5fa8c3847705c02e8a1cdf
ilhaskam <ilhaskam@gmail.com>
2024-06-26 11:33:21 +0700

    update Dockerfile node:14-alpine, add script build

---
+++ build_push_image_order-service.sh
@@ -0,0 +1,8 @@
+# command untuk build aplikasi di docker
+docker build -t ghcr.io/ilhamdwik/order-service:latest .
+
+# command untuk login docker hub di lokal vm kita
+echo PASSWORD_GITHUB_PACKAGES | docker login ghcr.io -u ilhamdwik --password-stdin
+
+# command untuk push image ke docker hub
+docker push ghcr.io/ilhamdwik/order-service:latest
